cb4dbf6371db004dd7cf1122c09153e8c9be7707
Valerijs Kanevs <valerijs.kanevs@icloud.com>
2022-07-20 22:50:14 +0900

    new commit

---
+++ script.js
@@ -0,0 +1,7 @@
+/* =================================== Typing animation =========================================*/
+var typed = new Typed(".typing", {
+    strings:["Musician","Singer","Songwriter","Astrophysicist","Inventor"],
+    typeSpeed:90,
+    BackSpeed:40,
+    loop:true
+})
